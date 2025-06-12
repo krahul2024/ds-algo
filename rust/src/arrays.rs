18cc7f33c8036c09a147f63b170958319e7ccf02
krahul2024 <culerrahul2024@gmail.com>
2025-06-12 20:59:29 +0530

    added another problem

--- rust/src/arrays.rs
+++ rust/src/arrays.rs
@@ -1,7 +1,30 @@
 use crate::utils::input_arr_int;
 
 pub fn main() {
-    pair_with_given_sum();
+    subarray_with_zero_sum();
+}
+
+fn subarray_with_zero_sum() {
+    // 3, 4, -1, 3, 1, 3, 1, -4, -2
+    // 3  7   6  9  10  13 14 10 found again, means there is subarray which has sum 0
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).unwrap();
+    let arr:Vec<i32> = buffer.trim().split_whitespace()
+        .map(|x| x.parse().unwrap()).collect();
+
+    let mut exists: std::collections::HashMap<i32, bool> = std::collections::HashMap::new();
+    let mut sum_so_far = 0;
+    for x in arr {
+        sum_so_far += x;
+        if exists.contains_key(&sum_so_far) && exists[&sum_so_far] {
+            println!("There exists a sub-array with sum 0");
+            return;
+        } else {
+            exists.insert(sum_so_far, true);
+        }
+    }
+
+    println!("There is no sub-array with sum 0");
 }
 
 fn pair_with_given_sum() {
