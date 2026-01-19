1b3a6a22b5ccd51c348b8769c11c5471a6d1f832
krahul <culerrahul2024@gmail.com>
2026-01-19 05:56:40 +0530

    stuck on a problem

---
+++ cses/dynamic-programming/java/rectangleCutting.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class rectangleCutting {
+    public static void main(String[] a) {
+        solve();
+    }
+
+    static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+    }
+
+
+}
